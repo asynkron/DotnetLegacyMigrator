762758ff011a9a394a284d8b40a15ffc9a3b9fbd
Roger Johansson <roger@asynkron.se>
2025-08-19 11:30:04 +0200

    Move relationship test data into expected files

--- tests/Translation.Tests/RelationshipMigrationTests.cs
+++ tests/Translation.Tests/RelationshipMigrationTests.cs
@@ -1,6 +1,5 @@
 using DotnetLegacyMigrator;
 using DotnetLegacyMigrator.Syntax;
-using DotnetLegacyMigrator.Models;
 using Microsoft.CodeAnalysis.CSharp;
 using System.IO;
 using Xunit;
@@ -12,27 +11,36 @@ public class RelationshipMigrationTests
     [Fact]
     public void LinqToSql_OneToMany_NavigationGenerated()
     {
-        var code = @"using System.Data.Linq;using System.Data.Linq.Mapping;[Table(Name=""Customers"")]public class Customer{[Column(IsPrimaryKey=true)]public int Id{get;set;}private EntitySet<Order> _Orders=new();[Association(Name=""FK_Orders_Customers"",ThisKey=""Id"",OtherKey=""CustomerId"")]public EntitySet<Order> Orders{get{return _Orders;}}}[Table(Name=""Orders"")]public class Order{[Column(IsPrimaryKey=true)]public int Id{get;set;}[Column]public int CustomerId{get;set;}private EntityRef<Customer> _Customer;[Association(Name=""FK_Orders_Customers"",ThisKey=""CustomerId"",OtherKey=""Id"",IsForeignKey=true)]public Customer Customer{get{return _Customer.Entity;}set{_Customer.Entity=value;}}}";
+        var code = File.ReadAllText(ExpectedPath("tests", "Translation.Tests", "Expected", "Relationships", "LinqToSql", "Model.cs.txt"));
         var tree = CSharpSyntaxTree.ParseText(code);
         var walker = new LinqToSqlEntitySyntaxWalker();
         walker.Visit(tree.GetRoot());
         var output = CodeGenerator.GenerateEntities(walker.Entities);
-        Assert.Contains("public List<Order> Orders { get; set; } = new();", output);
-        Assert.Contains("[ForeignKey(\"CustomerId\")]", output);
+        var expected = File.ReadAllText(ExpectedPath("tests", "Translation.Tests", "Expected", "Relationships", "LinqToSql", "Entities.txt"));
+        Assert.Equal(Normalize(expected), Normalize(output));
     }
 
     [Fact]
     public void NHibernate_ManyToMany_NavigationGenerated()
     {
-        var studentHbm = @"<hibernate-mapping xmlns=""urn:nhibernate-mapping-2.2""><class name=""Student"" table=""Students""><id name=""Id"" column=""Id"" type=""Int32""><generator class=""native""/></id><bag name=""Courses"" table=""StudentCourse""><key column=""StudentId""/><many-to-many class=""Course"" column=""CourseId""/></bag></class></hibernate-mapping>";
-        var courseHbm = @"<hibernate-mapping xmlns=""urn:nhibernate-mapping-2.2""><class name=""Course"" table=""Courses""><id name=""Id"" column=""Id"" type=""Int32""><generator class=""native""/></id><bag name=""Students"" table=""StudentCourse""><key column=""CourseId""/><many-to-many class=""Student"" column=""StudentId""/></bag></class></hibernate-mapping>";
+        var studentHbm = File.ReadAllText(ExpectedPath("tests", "Translation.Tests", "Expected", "Relationships", "NHibernate", "Student.hbm.xml"));
+        var courseHbm = File.ReadAllText(ExpectedPath("tests", "Translation.Tests", "Expected", "Relationships", "NHibernate", "Course.hbm.xml"));
         var f1 = Path.GetTempFileName();
         var f2 = Path.GetTempFileName();
         File.WriteAllText(f1, studentHbm);
         File.WriteAllText(f2, courseHbm);
         var (_, entities) = NHibernateHbmParser.ParseFiles(new[] { f1, f2 });
         var output = CodeGenerator.GenerateEntities(entities);
-        Assert.Contains("public List<Course> Courses { get; set; } = new();", output);
-        Assert.Contains("public List<Student> Students { get; set; } = new();", output);
+        var expected = File.ReadAllText(ExpectedPath("tests", "Translation.Tests", "Expected", "Relationships", "NHibernate", "Entities.txt"));
+        Assert.Equal(Normalize(expected), Normalize(output));
     }
+
+    private static string ExpectedPath(params string[] parts)
+    {
+        var root = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../../../"));
+        return Path.Combine(new[] { root }.Concat(parts).ToArray());
+    }
+
+    private static string Normalize(string input) => input.Replace("\r\n", "\n").Trim();
 }
+
